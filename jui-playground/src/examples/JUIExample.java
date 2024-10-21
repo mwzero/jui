9a4268e04b5731d7691afb8a3807e4ba4519d7ec
Maurizio Farina <maurizio.farina@gmail.com>
2024-10-21 16:06:04 +0200

    few fix over playground

---
+++ jui-playground/src/examples/JUIExample.java
@@ -0,0 +1,16 @@
+package src.examples;
+
+import static com.jui.JuiApp.jui;
+
+public class JUIExample {
+	
+	public static void main(String... args) throws FileNotFoundException, IOException {
+		
+		jui.input.header("Input Example", "blue");
+    	
+		//write here JUI code
+		jui.input.header("Input Example", "blue");
+		
+    	jui.start();
+    }
+}
--- jui-recipes/src/main/java/com/jui/recipes/MapZoomerRecipe.java
+++ jui-recipes/src/main/java/com/jui/recipes/MapZoomerRecipe.java
@@ -21,6 +21,4 @@ public class MapZoomerRecipe {
     	
     	jui.start();
 	}
-	
-
 }
