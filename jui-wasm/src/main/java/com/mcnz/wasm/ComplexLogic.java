8c10639fcf58700548569ce6f147fdde2ad5be59
Maurizio Farina <maurizio.farina@gmail.com>
2024-10-24 17:59:41 +0200

    few changes

--- jui-wasm/src/main/java/com/mcnz/wasm/ComplexLogic.java
+++ jui-wasm/src/main/java/com/mcnz/wasm/ComplexLogic.java
@@ -1,16 +1,35 @@
 package com.mcnz.wasm;
 
-import org.teavm.interop.*;
+//import static com.jui.JuiApp.jui;
+
+import org.teavm.interop.Export;
+import org.teavm.interop.Import;
 
 public class ComplexLogic {
 	
+	public static void main(String[] argv) {
+		
+	}
 	@Export(name = "getMagicNumber")
     public static void getMagicNumber(int range) {
 		int magicNumber = (range/2) + range%3;
 		setMagicNumber(magicNumber);
     }
 	
+	
 	@Import(module = "env", name = "setMagicNumber")
     private static native void setMagicNumber(int message);
 
+	/*
+	@Export(name = "getJUIHtml")
+    public static void getJUIHtml() {
+		
+		//jui.input.header("Input Example", "blue");
+		String content = "eccolo!"; //jui.render().toJsonString(); 
+		setHtml(content);
+    }
+
+	@Import(module = "env", name = "setHtml")
+    private static native void setHtml(String content);
+	 */
 }
