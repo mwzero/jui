79022f5054374c0249776b3588addf289c54b1ea
Maurizio Farina <maurizio.farina@gmail.com>
2024-12-04 17:23:50 +0100

    added Text Elements

--- jui-core/src/test/java/com/jui/apis/TextElements.java
+++ jui-core/src/test/java/com/jui/apis/TextElements.java
@@ -11,13 +11,32 @@ public class TextElements {
 	
 		log.info("Text Elements API");
 		
+		jui.title("This is a title text");
+		
+		jui.header("this is a header text");
+        
+        jui.subheader("this is a sub-header text");
+        
+        jui.divider();
+		
         jui.markdown("""
-        		# Text Elements APIs
+        		# this is markdown Text
         		""");
+        
+        jui.divider();
+        
+        jui.caption("this is a caption text");
 
         jui.code("""
         		System.out.println("Hello World");
         		""", "Java", true);
+        
+        
+        
+        jui.text("this is :blue[pre-formatted] text :sunglasses:");
+        
+        jui.html("<p><span style='text-decoration: line-through double red;'>Oops</span>!</p>");
+        
         
         jui.start();
 
