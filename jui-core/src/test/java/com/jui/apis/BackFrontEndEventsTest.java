603f9a741e3f524d5ffdf4d4d912194286ccf1b9
Maurizio Farina <maurizio.farina@gmail.com>
2024-12-22 20:23:23 +0100

    Update BackFrontEndEventsTest.java

--- jui-core/src/test/java/com/jui/apis/BackFrontEndEventsTest.java
+++ jui-core/src/test/java/com/jui/apis/BackFrontEndEventsTest.java
@@ -7,49 +7,27 @@ import lombok.extern.java.Log;
 @Log
 public class BackFrontEndEventsTest {
 
-	public static void main(String[] args) throws InterruptedException  { 
+	public static void main(String[] args) { 
 	
-		log.info("WS Channel Test");
-		
 		var my_bar = jui.progress(0, "Operation in progress. Please wait.");
 		
 		var btn = jui.button("Click-me", "primary");
 		btn.onClick("alert('click me')");
-		btn.onClick( (payload ) -> {
+		btn.onClick( () -> {
 
-            log.info("Payload: " + payload);
             btn.disable();
-			
 			for ( int i=1; i<=100; i++) {
-
-    			try {
+				try {
 					Thread.sleep(100);
-
+					my_bar.progress(i, "progress %d%%".formatted(i));
 				} catch (InterruptedException e) {
 					log.severe(e.getLocalizedMessage());
 				}
-    			my_bar.progress(i, "progress_text");
     		}
-        	my_bar.progress(0, "progress_text");
+			my_bar.progress(0, "progress 0%");
         	btn.enable();
         } );
 		
-		/*
-		var btn2 = jui.button("Click-me 2", "primary");
-		btn2.onClick( () -> {
-            System.out.println("Clicked!!");
-        } );
-        */
-		
-		/*
-		Thread.sleep(1000);
-		*/
-		//my_bar.empty();
-
-		//st.button("Rerun")
-		
-        //jui.spinner("Task in esecuzione").executeTask(task);
-        
         jui.start();
 
 	}
